9cdd4a35db16a30d56961a2eba6b58da712d5661
FourLineCode <akmal3535.ah@gmail.com>
2023-07-14 20:50:21 +0600

    svelete init with tailwind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<main class="flex flex-col items-center justify-center w-screen h-screen bg-gray-900">
+  <h1 class="text-2xl font-bold text-white">Welcome to SvelteKit</h1>
+  <p class="text-white">
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</main>
