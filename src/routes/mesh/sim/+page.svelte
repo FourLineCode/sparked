e0cff488bbc38433cf2bf813c2d157ab4adf45e5
FourLineCode <akmal3535.ah@gmail.com>
2023-07-16 02:54:48 +0600

    added mesh + node page + mesh model renderer

--- src/routes/mesh/sim/+page.svelte
+++ src/routes/mesh/sim/+page.svelte
@@ -1,1 +1,74 @@
-<h1>Mesh sim</h1>
+<script lang="ts">
+  import { setupScene } from '$lib/three-utils';
+  import { onMount } from 'svelte';
+  import ShowControlsButton from '../../../components/ShowControlsButton.svelte';
+
+  let controls = false;
+  function toggleControls() {
+    controls = !controls;
+  }
+
+  // let values = { voltage: 0, current: 0, resistance: 0 };
+  // const annotations: Record<keyof typeof values, ValueLabel | ButtonInput | null> = {
+  //   voltage: null,
+  //   current: null,
+  //   resistance: null,
+  // };
+
+  onMount(() => {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const { scene, loader, destroyScene } = setupScene(canvas);
+
+    loader.load('/models/mesh.glb', (gltf) => {
+      // gltf.scene.traverse((child) => {
+      //   if (Object.keys(values).includes(child.name)) {
+      //     const prop = child.name as keyof typeof values;
+      //     const object = createCssObject(child.position.x, child.position.y, child.position.z);
+
+      //     let svelteComponent: ValueLabel | ButtonInput | null = null;
+      //     if (prop === 'current') {
+      //       svelteComponent = new ValueLabel({
+      //         target: object.element,
+      //         props: { label: '' },
+      //       });
+      //     } else if (prop === 'resistance' || prop === 'voltage') {
+      //       svelteComponent = new ButtonInput({
+      //         target: object.element,
+      //         props: {
+      //           label: '',
+      //           onIncrement: () => (values = { ...values, [prop]: values[prop] + 1 }),
+      //           onDecrement: () => (values = { ...values, [prop]: values[prop] - 1 }),
+      //         },
+      //       });
+      //     }
+
+      //     annotations[prop] = svelteComponent;
+      //     scene.add(object);
+      //   }
+      // });
+
+      scene.add(gltf.scene);
+    });
+
+    return destroyScene;
+  });
+
+  // $: values = { ...values, current: values.voltage / values.resistance };
+
+  // $: for (const [key, element] of Object.entries(annotations)) {
+  //   if (key === 'current') {
+  //     element?.$set({
+  //       label: values.resistance === 0 ? '∞' : `${values[key].toFixed(2).replace(/\.00/g, '')} A`,
+  //     });
+  //   } else if (key === 'resistance' || key === 'voltage') {
+  //     element?.$set({ label: values[key] + (key === 'voltage' ? ' V' : ' Ω') });
+  //   }
+  // }
+
+  // $: Object.values(annotations).forEach((annotation) => annotation?.$set({ visible: controls }));
+</script>
+
+<main class="relative w-screen h-screen">
+  <ShowControlsButton show={controls} onClick={toggleControls} />
+  <canvas id="canvas" class="block w-full h-full" />
+</main>
--- src/routes/node/+page.svelte
+++ src/routes/node/+page.svelte
@@ -1,2 +1,99 @@
-<h1>Nodal analysis</h1>
-<a href="/node/sim">Start</a>
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import { Button } from '@svelteuidev/core';
+  import { slide } from 'svelte/transition';
+  import PlayIcon from '../../components/PlayIcon.svelte';
+
+  let step = 1;
+  function goNext() {
+    step += 1;
+  }
+
+  function gotoSimulation() {
+    goto('/node/sim');
+  }
+</script>
+
+<h1 class="mb-3 text-2xl font-bold text-center">Nodal analysis</h1>
+
+{#if step >= 1}
+  <div in:slide out:slide>
+    <!-- <p>
+      Mesh Current Analysis is a technique used to find the currents circulating around a loop or
+      mesh with in any closed path of a circuit.
+    </p>
+    <br />
+    <p>
+      As with any circuit analysis challenge, we have to solve a system of 2E independent equations,
+      where E is the number of circuit elements. The Mesh Current Method efficiently manages the
+      analysis task, resulting in a relatively small number of equations to solve. The Mesh Current
+      Method is based on Kirchhoff's Voltage Law (KVL).
+    </p>
+    <br /> -->
+  </div>
+{/if}
+
+{#if step >= 2}
+  <div in:slide out:slide>
+    <!-- <p>The Mesh Current Method uses two special terms: loop and mesh.</p>
+    <img src="/images/mesh.svg" alt="mesh" class="w-full h-auto" />
+    <br />
+    <p>
+      A loop is any closed path around a circuit. To trace a loop, you start at any component
+      terminal, and trace a path through connected elements until you get back to the starting
+      point. A loop is allowed to go through an element just one time. In the circuit above, there
+      are three loops, two solid loops, I and II, and one dashed loop III, all the way around the
+      outside.
+    </p>
+    <br /> -->
+  </div>
+{/if}
+
+{#if step >= 3}
+  <div in:slide out:slide>
+    <!-- <p>
+      Our circuit has two meshes. We identify two loop currents and call them i1 and i2. These are
+      going to be our independent variables. Important: the loop current directions are the same,
+      both flow in a clockwise direction.
+    </p>
+    <img src="/images/mesh2.svg" alt="mesh2" class="w-full h-auto" />
+    <p>
+      By defining a loop current in every mesh, you will have enough independent equations to solve
+      the circuit.
+    </p>
+    <br />
+    <p>Applying KVL we get the following equations:</p>
+    <br />
+    <Formula>
+      mesh 1: +5v - 2000 i1 - 1000 (i1 - i2) = 0
+      <br />
+      mesh 2: -1000 (i1 - i2) - 2000 i2 - 2V = 0
+    </Formula>
+    <br />
+    <p>Solving both equations we get following values for the mesh currents:</p>
+    <Formula>
+      i1 = -0.125 mA
+      <br />
+      i2 = +1.625 mA
+    </Formula>
+    <br /> -->
+  </div>
+{/if}
+
+{#if step >= 4}
+  <div in:slide out:slide>
+    <h2 class="text-xl font-bold">Visualize</h2>
+    <p>
+      You can visualize a physical representation of Nodal analysis in 3D. Start by clicking the
+      button below.
+    </p>
+    <Button color="green" fullSize radius="md" on:click={gotoSimulation}>
+      <PlayIcon slot="leftIcon" />
+      <span>Start simulation</span>
+    </Button>
+  </div>
+{/if}
+
+{#if step < 4}
+  <Button on:click={goNext} color="green" fullSize radius="md">Next</Button>
+{/if}
